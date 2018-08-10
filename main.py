69abf2a31db5830aa550b00304186e2f9b391d53
mkashifse <mk.kashif.se@gmail.com>
2018-08-10 18:08:23 +0500

    git pull

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-list = [1,2,2,3,3,]
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+	 return "<a href='test'>salam</a>"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
