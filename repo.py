6927b1d01e7981db1db76c0920fa667cb7fe3df4
mkashifse <mk.kashif.se@gmail.com>
2018-08-10 18:15:53 +0500

    git pull done

--- repo.py
+++ repo.py
@@ -1,3 +1,19 @@
 import os
+import threading
 
-os.system('git pull')
+
+
+
+def setInterval(func, sec):
+    def func_wrapper():
+        setInterval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def gitPull():
+	print('git pull')
+	os.system('git pull')
+
+setInterval(gitPull , 5)
